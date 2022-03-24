ab7a2a811750de1038ddaf9602222f70c1d4ba51
Rafael Linnankoski <rafaellinnankoski@gmail.com>
2022-03-24 13:18:09 +0900

    Add deletion of old backups

--- mongodb-cron-backups.sh
+++ mongodb-cron-backups.sh
@@ -4,7 +4,9 @@
 #
 
 # Styling
-pbold="\e[1;33m"
+punderline="\e[4m"
+pbold="\e[1m"
+pboldcolor="\e[1;33m"
 pclear="\e[0m"
 
 SCRIPTPATH=$(readlink -f "$0")
@@ -23,7 +25,7 @@ OUTPUT_PATH="${CUSTOM_OUTPUT_PATH:-"/home/$USER/MONGODB_DUMP/"}"
 create_backup(){
   mkdir -p $OUTPUT_PATH && cd $OUTPUT_PATH
   START_MESSAGE="Creating a backup of $CONNECTION using mongodump in $OUTPUT_PATH"
-  echo "\n${pbold}$START_MESSAGE${pclear}"
+  echo "\n${pboldcolor}$START_MESSAGE${pclear}"
   logger $START_MESSAGE
 
   START=$(date +%s)
@@ -31,7 +33,7 @@ create_backup(){
 
   # Loop through DBs to backup
   for db in $DB_NAMES; do
-    echo "\n${pbold}Backing up $db...${pclear}"
+    echo "\n${pboldcolor}Backing up $db...${pclear}"
     
     URI="mongodb+srv://$USR:$PSWD@$CONNECTION/$db"
     FILENAME="$( date +%Y-%m-%d-%H-%M-%S ).$db.archive.gz"
@@ -52,24 +54,53 @@ create_backup(){
   TIME=$(($END - $START))
   
   END_MESSAGE="Backups created for $CONNECTION (took "$TIME" s, total size $TOTAL_SIZE B)"
-  printf "\n${pbold}$END_MESSAGE${pclear}\n\n"
+  printf "\n${pboldcolor}$END_MESSAGE${pclear}\n\n"
   logger $END_MESSAGE
 }
 
-# Delete old backups?
-# Retain e.g. backups from last 7 days and delete rest
+# Delete old backups
+# Retain e.g. backups from last X (default: 7) days and delete rest
+delete_old_backups(){
+  cd $OUTPUT_PATH
+  find -ctime +"${RETAIN_LAST_X_DAYS_BACKUPS:-"7"}" -delete
+  DEL_MESSAGE="Backups older than "${RETAIN_LAST_X_DAYS_BACKUPS:-"7"}" days deleted for $CONNECTION."
+  printf "\n${pboldcolor}$DEL_MESSAGE${pclear}\n\n"
+  logger $DEL_MESSAGE
+}
+
+create_and_delete(){
+  create_backup
+  delete_old_backups
+}
 
 help(){
   printf \
 "
-${pbold}Tool to create MongoDB backups from your MongoDB Atlas database through commandline allowing
+${pboldcolor}Tool to create MongoDB backups from your MongoDB Atlas database through commandline allowing
 CRON scheduling.${pclear}
 
-Requirements: 
-    - mongodump from mongodb-database-tools
+${punderline}Requirements${pclear}
+    ${pbold}mongodump${pclear} (from mongodb-database-tools)
         - Get a copy at https://www.mongodb.com/try/download/database-tools
-    - env vars:
-        - add a file named 'config' in the script folder (use the example_config to create one)
+
+    ${pbold}environment variables${pclear}
+        - Add a file named 'config' in the same folder with the script
+        - You can use the example_config to create the config.
+
+${punderline}Commands${pclear}
+Run ${pbold}mongobackup [action]${pclear} (or whatever name you specified instead of 'mongobackup' when creating the symlink to your \$PATH). 
+
+  ${pbold}create${pclear}
+      Creates backups for the databases defined in the config file.
+
+  ${pbold}delete${pclear}
+      Delete backups older than X days (as specified in the config file; defaults to 7, if not set).
+
+  ${pbold}createdelete${pclear}
+      Run the two above commands in sequence.
+
+  ${pbold}help${pclear}
+      Display this help file.
 \n"
   exit
 }
@@ -77,6 +108,10 @@ Requirements:
 case "$1" in
   "create")
     create_backup;;
+  "delete")
+    delete_old_backups;;
+  "createdelete")
+    create_and_delete;;
   *)
     help;;
 esac
